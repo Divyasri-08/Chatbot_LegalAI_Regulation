ec4a24cb899b81fd00aa19d6050f3573b7f138e0
Divyasri-08 <divyasrimunnangi09@gmail.com>
2025-12-28 00:41:30 -0600

    Update new.py

--- Chatbot_LegalAI_Regulation/new.py
+++ Chatbot_LegalAI_Regulation/new.py
@@ -1,3 +1,3 @@
-print("first need to check status git status")
+print("first need to check the status git status")
 print("then to add a new file need to write git add dot")
 print("then write git commit minus m new dot py in double quotes")
